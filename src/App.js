7ea94ad3aa3cc021bacb0ad57c2afa697267abc6
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-03-21 18:22:35 +0000

    Navigation Layout and base About section created

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import React, { useState, Fragment } from "react";
 
 import "antd/dist/reset.css";
 
-import NavigationBar from "./components/navigation-bar";
+import NavigationBar from "./components/NavigationBar";
+import About from "./components/About";
+
+// Ant Design
 import { ConfigProvider } from "antd";
-import Project from "./components/project";
+import { Button, DatePicker, Space, version } from "antd";
+
+// Components
+import Project from "./components/Project";
+import MyButton from "./components/Button";
+
+// Theme
+import ThemeContext from "./ThemeContext.js";
 
 function App() {
   return (
     <Fragment>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: "#00b96b",
-          },
-        }}
-      >
-        {/* <NavigationBar /> */}
-        <Project />
+      <ConfigProvider>
+        <ThemeContext.Provider
+          value={{
+            token: {
+              colorPrimary: "#722ED1",
+            },
+          }}
+        >
+          <main className="App">
+            <NavigationBar />
+            <About />
+            <Project />
+            <MyButton text="Hello" />
+          </main>
+        </ThemeContext.Provider>
       </ConfigProvider>
+
+      <div className="App">
+        <h1>antd version: {version}</h1>
+        <Space>
+          <DatePicker />
+          <Button type="primary">Primary Button</Button>
+        </Space>
+      </div>
     </Fragment>
   );
 }
